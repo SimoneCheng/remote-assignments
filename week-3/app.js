d66ab32a4969e0c8ec664d6130b5c7adc4437f11
SimoneCheng <bleach1827@gmail.com>
2021-08-13 15:13:07 +0800

    寫assignment1&2

--- week-3/app.js
+++ week-3/app.js
@@ -1,5 +1,24 @@
 const express = require('express');
-
 const app = express();
+
+// assignment 1: your first web server
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to localhost:3000</h1>');
+});
+
+// assignment 2: build backend API for front-end
+app.get('/getData/', (req, res) => {
+    let result;
+    if (!req.query.number) {
+        result = "<h1>Lack of Parameter</h1>";
+    } else {
+        if (isNaN(req.query.number)) {
+            result = "<h1>Wrong Parameter</h1>";
+        } else {
+            result = ((1 + Number(req.query.number)) * Number(req.query.number)) / 2;
+        }
+    }
+    res.send(`${result}`);
+});
 
 app.listen(3000);
